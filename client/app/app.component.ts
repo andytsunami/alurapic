6059228c12de38e615e8d32ea100f27b6610dc4c
André Vasconcelos <gp-andre.vasconcelos@uol.com.br>
2017-08-01 10:34:12 -0300

    Injeção de dependencia do httpModule

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,8 +1,15 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
+import { Http } from '@angular/http'
 
 @Component({
     moduleId: module.id,
     selector:   'app',
     templateUrl:    './app.component.html'
 })
-export class AppComponent{}
+export class AppComponent{
+    fotos: Object[] = [];
+
+    constructor(http: Http){
+
+    }
+}
